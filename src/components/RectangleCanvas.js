042f49253ce405dbb50dc0f023bcec95a6a3e429
CGSnider <cgsnider@outlook.com>
2020-07-12 18:20:07 -0400

    first commit

---
+++ src/components/RectangleCanvas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class RectangleCanvas extends Component {
+    render() {
+        return (
+            <canvas></canvas>
+        )
+    }
+}
+
+export default RectangleCanvas
---
+++ src/components/SelectionBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SelectionBar extends Component {
+    render() {
+        return (
+            <div>
+                Options
+            </div>
+        )
+    }
+}
+
+export default SelectionBar
